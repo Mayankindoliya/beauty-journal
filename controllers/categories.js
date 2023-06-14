888da193ff539be94529b0476a5d6c01d0981c9a
Mayank <mayankindoliya@gmail.com>
2023-06-14 16:46:42 +0530

    categories controllers and routes created.

--- controllers/categories.js
+++ controllers/categories.js
@@ -3,7 +3,15 @@ const Categories = require('../models/categories');
 class CategoriesControllers {
 
   static async createCategories(document) {
-   return Categories.create(document)
+    return Categories.create(document)
+  };
+
+  static async readCategories(criteria) {
+     return Categories.find(criteria.filter, criteria.fields, criteria.option).lean()
+  };
+
+  static async updateCategories(document, id) {
+   return Categories.findOneAndUpdate({_id: id}, document, {new: true}).lean()
   };
 
 };
--- routes/categories.js
+++ routes/categories.js
@@ -5,6 +5,26 @@ const CategoriesControllers = require('../controllers/categories');
 
 router.post('/categories/create', (req, res, next) => {
   CategoriesControllers.createCategories(req.body)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
+});
+
+router.get('/categories/read', (req, res, next) => {
+  CategoriesControllers.readCategories(req.criteria)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      next(err)
+    })
+});
+
+router.put('/categories/update/:id', (req, res, next) => {
+  CategoriesControllers.updateCategories(req.body, req.params.id)
   .then((data) => {
     res.json(data)
   })
